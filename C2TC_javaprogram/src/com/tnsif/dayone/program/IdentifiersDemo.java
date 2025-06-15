4129f0f8c0b7e6245936b4db6cdd40cd7a45386b
ELCOT <ELCOT@ARCHANA>
2025-06-15 01:11:14 -0700

    dayone java program

---
+++ C2TC_javaprogram/src/com/tnsif/dayone/program/IdentifiersDemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.dayone.program;
+
+public class IdentifiersDemo {
+
+	public static void main(String[] args) {
+				
+				int $number = 20; 
+				System.out.println("value of the number variable is : "+ $number);
+			
+				String studentName="Abitha";
+				System.out.println("value is : "+ studentName);
+
+	}
+
+}
